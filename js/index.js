1e12c0d54a0a78287e5a8ffacb1843e80796d119
SaraAzmin <azmin.sara17@gmail.com>
2022-02-28 18:25:33 +0100

    error handled and no search result shown

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+const searchSection = document.getElementById("search-section");
 const displaySection = document.getElementById("phone-display-section");
 
 //onclick function for the search button
@@ -5,25 +6,46 @@ const searchButton = () => {
     const input = document.getElementById("phone-name-input");
     const inputValue = input.value;
 
-    //dynamic url for searching by input
-    fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
-        .then(res => res.json())
-        .then(data => displayPhones(data.data))
+    //error checking
+    if (inputValue == '') {
+        alert("Please input phone name");
+        displaySection.innerHTML = '';
+    }
+    else if (!isNaN(inputValue)) {
+        alert("Please give a valid input");
+        input.value = '';
+    }
+    else {
+        //dynamic url for searching by input
+        fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
+            .then(res => res.json())
+            .then(data => displayPhones(data.data))
+    }
 }
 
 //function for displaying the phones searched for
 const displayPhones = (phones) => {
 
-    for (const phone of phones) {
-        const gridDiv = document.createElement("div");
-        gridDiv.className = "border-2 border-cyan-700 rounded-md text-center flex flex-col justify-center items-center py-3";
-        gridDiv.innerHTML = `
-        <img class=" h-2/3" src="${phone.image}" alt="">
-            <h1 class="uppercase mt-3 text-xl">${phone.brand}</h1>
-            <p class="my-3">${phone.phone_name}</p>
-            <button class="bg-cyan-700 text-white py-2 px-3 lg:px-5 font-semibold rounded-md">Show Details</button>
+    //show if no results found
+    if (phones.length == 0) {
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <h4 class="text-2xl text-cyan-700 font-bold text-center mt-5">Found no results for search</h4>
         `
-        displaySection.appendChild(gridDiv);
+        searchSection.appendChild(div);
+    }
+    else {
+        for (const phone of phones) {
+            const gridDiv = document.createElement("div");
+            gridDiv.className = "border-2 border-cyan-700 rounded-md text-center flex flex-col justify-center items-center py-3";
+            gridDiv.innerHTML = `
+            <img class=" h-2/3" src="${phone.image}" alt="">
+                <h1 class="uppercase mt-3 text-xl">${phone.brand}</h1>
+                <p class="my-3">${phone.phone_name}</p>
+                <button class="bg-cyan-700 text-white py-2 px-3 lg:px-5 font-semibold rounded-md">Show Details</button>
+            `
+            displaySection.appendChild(gridDiv);
+        }
     }
 
 }
